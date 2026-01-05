77055aaa3767465bcafa12d3b294290e6bee6d0d
Recep Demir <recep.demir@powercoders.org>
2026-01-05 14:57:40 +0100

    feat: add initial code structure and input handling in task_1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,26 @@
-numbers = [12, 5, 8, 19, 3, 20, 2, 14, 7, 6]
+# # # name = "John"
+# # # character_age = "35"
+
+# # # print(name.lower())
+# # # print(len(name))
+# # # print(name.index("o"))
+
+# # age = 50
+# # gpa = 3.7
+
+# # print(pow(2,3))
+# # print(2**3)
+# # print(round(gpa))
+# # print(max(7,3))
+
+# # name = input("What is your name? ")
+# # print("Hello " + name)
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2)
+# print(result)
+
+
+sayilar = [12, 5, 8, 19, 3, 20, 2, 14, 7, 6]
+# Görevin: Bu listedeki çift sayıları bulan, karelerini alan ve büyükten küçüğe sıralayan o tek satırlık List Comprehension kodunu yaz.
