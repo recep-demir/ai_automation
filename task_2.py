3933237fb0363db20b8c3325eee7fa23bbe2a6d8
Recep Demir <recep.demir@powercoders.org>
2026-01-07 14:39:37 +0100

    refactor: fix formatting in task_2.py for consistency

--- task_2.py
+++ task_2.py
@@ -2,8 +2,7 @@ file_path = "server.log"
 error_list = []
 search_word = "ERROR"
 
-
-with open(file_path, "r", encoding="utf-8") as file:
+with open (file_path, "r", encoding="utf-8") as file:
     for line in file:
         if search_word in line:
             error_list.append(line.strip())
