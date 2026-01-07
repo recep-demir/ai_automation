91d744c650271b465dc0ba50ee769366d66e9ab2
Recep Demir <recep.demir@powercoders.org>
2026-01-07 13:21:17 +0100

    refactor: comment out unused code and maintain number input functionality

--- task_yt.py
+++ task_yt.py
@@ -2,9 +2,17 @@
 # weight_kg = 0.4536 * weight_p
 # print(weight_kg)
 
-first = 'John'
-last = 'Smith'
-message = first + ' [' + last + '] is a code '
-msg = f'{first} [{last}] is a coder'
-print (message)
-print (msg)
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a code '
+# msg = f'{first} [{last}] is a coder'
+# print (message)
+# print (msg)
+
+number = int(input ("Please enter a number : "))
+
+if number > 10 :
+    print("entered big number")
+
+else :
+    print("entered small number")
