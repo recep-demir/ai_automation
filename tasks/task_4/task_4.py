d3a12cd719e1f1fc31eeab1a1007078684b5899d
Recep Demir <recep.demir@powercoders.org>
2026-01-15 15:43:57 +0100

    fix: improve error handling for request exceptions in task_4.py

--- tasks/task_4/task_4.py
+++ tasks/task_4/task_4.py
@@ -22,6 +22,8 @@ try:
     else:
         print(f"Server returned an error:{response.status_code}")
             
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while making the request: {e}")
 
 except Exception as e:
     print(f"An error occurred while connecting to the API: {e}")
