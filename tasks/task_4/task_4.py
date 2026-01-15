df638ebb971e59059cd6cf9b9b581da46bf391ef
Recep Demir <recep.demir@powercoders.org>
2026-01-15 15:36:13 +0100

    Add Day 4: API integration and service health checker

---
+++ tasks/task_4/task_4.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+url = "https://jsonplaceholder.typicode.com/todos/1"
+
+
+try:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Data: {data}")
+
+        is_completed = data["completed"]
+
+        if is_completed == False:
+            print("ALARM: Task not completed!")
+
+        else:
+            print("System Stabil")
+    
+    else:
+        print(f"Server returned an error:{response.status_code}")
+            
+
+except Exception as e:
+    print(f"An error occurred while connecting to the API: {e}")
+    sys.exit(1)
