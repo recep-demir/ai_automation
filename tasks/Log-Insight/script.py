55ac94a2cd886403791b2852ab7cf2b06cbeb6c0
Recep Demir <recep.demir@powercoders.org>
2026-01-20 13:35:08 +0100

    feat: implement service connection using SECURITY_API_URL from environment variables

--- tasks/Log-Insight/script.py
+++ tasks/Log-Insight/script.py
@@ -12,3 +12,15 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
+api_url = os.getenv("SECURITY_API_URL")
+
+def connect_to_service():
+    if not api_url:
+        print("Error:Api_url not found in enviroment variables!")
+        return
+    
+    else:
+        print(f"Connecting to service using: {api_url}...")
+
+connect_to_service()
