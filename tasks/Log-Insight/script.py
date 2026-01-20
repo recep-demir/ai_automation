f5c465eefdb6592b6e96cbe99474470e36f7506e
Recep Demir <recep.demir@powercoders.org>
2026-01-20 16:38:02 +0100

    feat: add error handling for file input and log critical errors with source IP

--- tasks/Log-Insight/script.py
+++ tasks/Log-Insight/script.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import re
 import logging
 from dotenv import load_dotenv
 
@@ -28,3 +30,23 @@ def connect_to_service():
         logging.info("SECURITY_API_URL environment variable is set.")
 
 connect_to_service()
+
+file_name = sys.argv[1]
+
+if not os.path.exists(file_name):
+    logging.error(f"Error: The file '{file_name}' was not found!")
+    sys.exit(1)
+
+try:
+    with open(file_name,"r",encoding="utf-8") as file:
+        for line in file:
+            match = re.search(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", line)
+            if "ERROR" in line and match:
+                ip_address = match.group(1)
+                logging.error(f"Crirical Error! Source IP: {ip_address}")
+
+
+
+except Exception as e:
+    logging.error(f"An error occurred: {e}") 
+    sys.exit()
