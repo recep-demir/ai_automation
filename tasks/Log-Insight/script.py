6d2dc09ceb409d91f7e286ede77eb93fb71f07aa
Recep Demir <recep.demir@powercoders.org>
2026-01-20 15:34:38 +0100

    feat: update logging configuration to use dynamic log file path and improve error handling

--- tasks/Log-Insight/script.py
+++ tasks/Log-Insight/script.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+current_location = os.getcwd()
+target_path = os.path.join(current_location,"tasks","Log-Insight","app.log")
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),
+        
+        logging.FileHandler(target_path),
         logging.StreamHandler()
     ]
 )
@@ -17,10 +21,10 @@ api_url = os.getenv("SECURITY_API_URL")
 
 def connect_to_service():
     if not api_url:
-        print("Error:Api_url not found in enviroment variables!")
+        logging.error("SECURITY_API_URL environment variable is not set.")
         return
     
     else:
-        print(f"Connecting to service using: {api_url}...")
+        logging.info("SECURITY_API_URL environment variable is set.")
 
 connect_to_service()
