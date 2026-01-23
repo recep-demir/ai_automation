6f67104aadc016d60c934eb6df13a1ad422d73f3
Recep Demir <recep.demir@powercoders.org>
2026-01-23 23:57:57 +0100

    feat: implement initial log processing functionality with error handling

--- project/log_insight.py
+++ project/log_insight.py
@@ -19,3 +19,31 @@ logging.basicConfig(
     ]
 )
 
+API_URL = os.getenv("API_URL")
+
+
+
+
+def process_logs():
+    if len(sys.argv) < 2 :
+        logging.error("Usage:python <script_name> <log_file_path>")
+        sys.exit(1)
+
+    if not os.path.exists(sys.argv(1)):
+        logging.error(f"Input file not found: {sys.argv(1)}")
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+
+    try:
+        with open (file_name,"r",encoding="utf-8") as file:
+            for line in file:
+                ip_match = re.search(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", line)
+                time_match = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})", line)
+
+                if 
+
+
+    
+
+
