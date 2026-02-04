f4db6f47e5fccd04fe2074204e071c4629caea01
Recep Demir <recep.demir@powercoders.org>
2026-02-04 16:36:01 +0100

    fix: correct logging configuration and remove unnecessary lines

--- project/AI-Insight-Integration/ai_log_insight.py
+++ project/AI-Insight-Integration/ai_log_insight.py
@@ -11,9 +11,12 @@ JSON_ALERT_FILE = os.path.join(LOG_DIR,"security_alert.json")
 
 os.makedirs(LOG_DIR,exist_ok=True)
 
-
-
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler(APP_LOG_FILE), logging.StreamHandler()]
+)
 
 class SecurityLogAnalyer:
-    def __init__(self,log_file)
+    def __init__(self,)
         
