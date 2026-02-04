adf930c6e009f3292bd5fb814b4abe6e4a577293
Recep Demir <recep.demir@powercoders.org>
2026-02-04 14:34:41 +0100

    feat: restructure log file paths and initialize SecurityLogAnalyzer class

--- project/AI-Insight-Integration/ai_log_insight.py
+++ project/AI-Insight-Integration/ai_log_insight.py
@@ -4,6 +4,11 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
-client = Groq(
-    api_key = os.environ.get("GROQ_API_KEY")
-)
+
+LOG_DIR = os.path.join(os.getcwd(),"project","AI-Insight-Integration")
+APP_LOG_FILE = os.path.join(LOG_DIR,"app.log")
+JSON_ALERT_FILE = os.path.join(LOG_DIR,"security_alert.json")
+
+class SecurityLogAnalyer:
+    def __init__(self,log_file)
+        
