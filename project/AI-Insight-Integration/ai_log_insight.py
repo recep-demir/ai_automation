53cfcd448a9ba778a495ae607e434a3b1ef1ecc0
Recep Demir <recep.demir@powercoders.org>
2026-02-04 16:19:24 +0100

    fix: remove redundant log directory creation

--- project/AI-Insight-Integration/ai_log_insight.py
+++ project/AI-Insight-Integration/ai_log_insight.py
@@ -9,6 +9,11 @@ LOG_DIR = os.path.join(os.getcwd(),"project","AI-Insight-Integration")
 APP_LOG_FILE = os.path.join(LOG_DIR,"app.log")
 JSON_ALERT_FILE = os.path.join(LOG_DIR,"security_alert.json")
 
+os.makedirs(LOG_DIR,exist_ok=True)
+
+
+
+
 class SecurityLogAnalyer:
     def __init__(self,log_file)
         
